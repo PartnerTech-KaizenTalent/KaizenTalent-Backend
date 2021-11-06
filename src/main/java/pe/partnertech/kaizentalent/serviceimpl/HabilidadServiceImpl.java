be2dab27c19ae7ce4d53ce532c32f93a18e131af
diegohcs96 <diego.hcs.96@gmail.com>
2021-11-05 19:22:51 -0500

    Create HabilidadServiceImpl.java

---
+++ src/main/java/pe/partnertech/kaizentalent/serviceimpl/HabilidadServiceImpl.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021. Developed by Diego Campos Sandoval.
+ */
+
+package pe.partnertech.kaizentalent.serviceimpl;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class HabilidadServiceImpl {
+}
